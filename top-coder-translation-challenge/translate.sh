10eb34af0628af288160133acec048b4e41bb453
nic-instruction <nicole.bade@seattlecolleges.edu>
2021-05-06 13:34:26 -0700

    Create translate.sh

---
+++ top-coder-translation-challenge/translate.sh
@@ -0,0 +1,55 @@
+#!/bin/bash
+
+# https://cloud.google.com/translate/docs/reference/rest/v3beta1/projects/translateText
+# https://cloud.google.com/translate/docs/basic/translating-text
+
+if [ ! -n "$1" ]; then
+   echo -e "\nPlease enter text to translate as an argument\n\nSyntax: translate.sh my line of text for translation\n"
+   exit 0;
+fi
+
+# note: $# -eq 0  would also work $# gives number of arguments.
+# -z would work instead of ! -n.
+
+# enable google translate
+gcloud services enable translate.googleapis.com
+
+# store project id in a variable
+export PROJECT_ID=$(gcloud config get-value core/project)
+
+# create a service account for translation
+gcloud iam service-accounts create my-translation-sa \
+  --display-name "my translation service account"
+
+# grant the service account the translation role
+gcloud projects add-iam-policy-binding ${PROJECT_ID} \
+  --member serviceAccount:my-translation-sa@${PROJECT_ID}.iam.gserviceaccount.com \
+  --role roles/cloudtranslate.user
+
+# download service account credentials to ~/key.json
+
+gcloud iam service-accounts keys create ~/key.json \
+  --iam-account my-translation-sa@${PROJECT_ID}.iam.gserviceaccount.com
+
+# set the app credentials variable
+
+export GOOGLE_APPLICATION_CREDENTIALS=~/key.json
+
+
+text_to_translate="$@"
+
+# Make a json file with our text to translate and our target language.
+
+echo '{
+  "q": [''"$@"''],
+  "target": "de"
+}' > request.json
+
+# Use curl to make a post request that will provide an auth token (from the auth we set up) and send the payload 
+# from the json file.  
+
+curl -X POST \
+-H "Authorization: Bearer "$(gcloud auth application-default print-access-token) \
+-H "Content-Type: application/json; charset=utf-8" \
+-d @request.json \
+https://translation.googleapis.com/language/translate/v2
